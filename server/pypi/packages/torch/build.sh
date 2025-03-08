daa3c844ac1fb198f23d3220483ec2f0d0e40eaf
Xiao Zhang <xiaoz@comoco.xyz>
2025-03-08 13:39:37 +0800

    update patch

--- server/pypi/packages/torch/build.sh
+++ server/pypi/packages/torch/build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -eu
 
+source_dir=$PWD/third_party/sleef
 build_sleef_native=$PWD/build-sleef-native
 
-cd third_party/sleef
-cmake -S . -B $build_sleef_native
+cmake -S $source_dir -B $build_sleef_native
 cmake --build $build_sleef_native -j --clean-first
