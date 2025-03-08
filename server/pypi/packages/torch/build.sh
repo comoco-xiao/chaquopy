9b9a4e4ae4739f5e14a40f8f4e6ba8d8cf40a025
Xiao Zhang <xiaoz@comoco.xyz>
2025-03-08 14:25:11 +0800

    update patch

--- server/pypi/packages/torch/build.sh
+++ server/pypi/packages/torch/build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-set -eu
+# set -eu
 
-source_dir=$PWD/third_party/sleef
-build_sleef_native=$PWD/build-sleef-native
+# source_dir=$PWD/third_party/sleef
+# build_sleef_native=$PWD/build-sleef-native
 
-unset AR ARFLAGS AS CC CFLAGS CPP CPPFLAGS CXX CXXFLAGS F77 F90 FARCH FC LD LDFLAGS LDSHARED \
-      NM RANLIB READELF STRIP CMAKE_TOOLCHAIN_FILE
+# unset AR ARFLAGS AS CC CFLAGS CPP CPPFLAGS CXX CXXFLAGS F77 F90 FARCH FC LD LDFLAGS LDSHARED \
+#       NM RANLIB READELF STRIP CMAKE_TOOLCHAIN_FILE
 
-cmake -S $source_dir -B $build_sleef_native
-cmake --build $build_sleef_native -j --clean-first
+# cmake -S $source_dir -B $build_sleef_native
+# cmake --build $build_sleef_native -j --clean-first
